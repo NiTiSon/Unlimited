3f73ce5fc46564a28c779874152af9b34394a0dc
NiTiSon <NiTiSon@users.noreply.github.com>
2026-06-16 02:17:18 +0300

    first commit

---
+++ Unlimited/IntX.cs
@@ -0,0 +1,10 @@
+namespace Unlimited
+{
+	/// <summary>
+	/// Represents an unlimited signed integer.
+	/// </summary>
+	public sealed class IntX
+	{
+
+	}
+}
---
+++ Unlimited/UIntX.cs
@@ -0,0 +1,10 @@
+namespace Unlimited
+{
+	/// <summary>
+	/// Represents an unlimited unsigned integer.
+	/// </summary>
+	public sealed class UIntX
+	{
+
+	}
+}
